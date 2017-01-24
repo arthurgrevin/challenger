b25168adbda6e8e2f32161319a7a2891d7ece72e
smngrvn <simon.grevin@gmail.com>
2017-01-24 21:43:32 +0000

    add end_date + make every uri work

--- api/insert_data.sh
+++ api/insert_data.sh
@@ -1,9 +1,9 @@
 #
 # insert 3 challenges
 #
-curl http://localhost:5000/challenges/ -H "Content-Type: application/json" -d '{"title": "My first challenge"}' -X POST
-curl http://localhost:5000/challenges/ -H "Content-Type: application/json" -d '{"title": "My second challenge"}' -X POST
-curl http://localhost:5000/challenges/ -H "Content-Type: application/json" -d '{"title": "My third challenge", "days":3, "start_date":"2017-01-23"}' -X POST
+curl http://localhost:5000/challenges/ -H "Content-Type: application/json" -d '{"title": "My first challenge", "days":3, "start_date":"2017-01-23", "end_date":"2017-01-26"}' -X POST
+curl http://localhost:5000/challenges/ -H "Content-Type: application/json" -d '{"title": "My second challenge", "days":3, "start_date":"2017-01-23", "end_date":"2017-01-26"}' -X POST
+curl http://localhost:5000/challenges/ -H "Content-Type: application/json" -d '{"title": "My third challenge", "days":3, "start_date":"2017-01-23", "end_date":"2017-01-26"}' -X POST
 #
 # get all challenges
 # curl http://localhost:5000/challenges/
--- api/resources/challenge.py
+++ api/resources/challenge.py
@@ -2,28 +2,34 @@ from flask_restful import Resource
 from flask_restful import reqparse, fields, marshal_with
 from datetime import datetime
 
-challenges = {}
+challenges = []
 seq = 0
 
 parser = reqparse.RequestParser()
 parser.add_argument('title', location = 'json', help = 'Challenge title')
 parser.add_argument('days', type = int, location = 'json', help = 'Number of days for the challenge')
 parser.add_argument('start_date', type = lambda x: datetime.strptime(x, '%Y-%m-%d'), location = 'json', help = 'Challenge start date')
+parser.add_argument('end_date', type = lambda x: datetime.strptime(x, '%Y-%m-%d'), location = 'json', help = 'Challenge end date')
 
+# marshaller
 challenge_fields = {
+    'id': fields.Integer,
     'title': fields.String,
     'days': fields.Integer,
-    'start_date': fields.DateTime(dt_format='rfc822')
+    'start_date': fields.DateTime(dt_format='rfc822'),
+    'end_date': fields.DateTime(dt_format='rfc822')
 }
 challenge_list_fields = {
     fields.List(fields.Nested(challenge_fields)),
 }
+
 
 # sequence of ids
 def get_id():
     global seq
     seq += 1
     return seq
+
 
 class Challenges(Resource):
     @marshal_with(challenge_fields, envelope='challenge')
@@ -35,19 +41,33 @@ class Challenges(Resource):
     def post(self):
         args = parser.parse_args()
         id = get_id()
-        challenge = {"id": id, "title": args['title'], "days": args['days'], "start_date": args['start_date']}
-        challenges[id] = challenge
+        
+        # create challenge object
+        challenge = {
+            "id": id, 
+            "title": args['title'], 
+            "days": args['days'], 
+            "start_date": args['start_date'],
+            "end_date": args['end_date']
+        }
+        
+        # add to challenges
+        challenges.append(challenge)
         return challenge, 201
+
 
 class Challenge(Resource):
 
     @marshal_with(challenge_fields, envelope='challenge')
     def get(self, challenge_id):
         # return the challenge based on id
-        return challenges[challenge_id]
+        return [challenge for challenge in challenges if challenge['id'] == challenge_id]
         
     def delete(self, challenge_id):
         #delete the challenge
-        del challenges[challenge_id]
+        for challenge in challenges[:]:
+            if challenge['id'] == challenge_id:
+                # do something with item
+                challenges.remove(challenge)
         return '', 204
 
