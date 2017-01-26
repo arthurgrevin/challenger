f9965b386ec376ffd36d0467c5bcf5a6ac3c6a68
smngrvn <simon.grevin@gmail.com>
2017-01-26 20:33:21 +0000

    improve tests

--- api/test.py
+++ api/test.py
@@ -3,16 +3,30 @@ import nose
 from nose.tools import *
 from api import app
 
-def check_content_type(headers):
-    eq_(headers['Content-Type'], 'application/json')
 
-test_app = app.test_client()
-rv = test_app.get('/challenges/')
-#check_content_type(rv.headers)
-resp = json.loads(rv.data)
 
-# make sure we get a response
-eq_(rv.status_code, 200)
-
-# make sure there are no users
-eq_(len(resp['challenge']), 0)
+def test_challenges():
+    test_app = app.test_client()
+    
+    
+    ## 1 ## GET returns nothing
+    
+    rv = test_app.get('/challenges/')
+    resp = json.loads(rv.data)
+    eq_(rv.status_code, 200)
+    eq_(len(resp['challenge']), 0)
+    
+    
+    ## 2 ## POST creates a challenge
+    #d = dict(title="Coding challenge", days=34)
+    #rv = test_app.post('/challenges/', data=d)
+    #eq_(rv.status_code, 201)
+    
+    
+    ## 3 ## GET returns 1 challenge
+    
+    #rv = test_app.get('/challenges/')
+    #resp = json.loads(rv.data)
+    #eq_(rv.status_code, 200)
+    #eq_(len(resp['challenge']), 1)
+    #eq_(resp['challenge'][0], "Coding challenge")
