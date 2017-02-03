c54aa4224a4b8c44208e461d1f9239505ca40979
smngrvn <simon.grevin@gmail.com>
2017-02-03 21:26:25 +0000

    date util to get days between two dates

---
+++ api/resources/date_util.py
@@ -0,0 +1,18 @@
+from datetime import date, timedelta
+
+# get an array of all days between two dates
+def get_all_dates_between(start, end):
+    
+    # number of days between the two dates
+    nb_days = (end-start).days
+    
+    days = []
+    
+    for i in range(0, nb_days+1):
+        days.append(start + timedelta(days=i))
+    
+    return days
+        
+
+days = get_all_dates_between(date(2011,11,17), date(2011,11,24))
+print(days)
