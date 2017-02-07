a4442b0e392aa2f180a112a30779aee5c371d42f
ARTHUR GREVIN <arthur.grevin@gmail.com>
2017-02-07 23:10:34 +0100

    user models

--- api/run.py
+++ api/run.py
@@ -1,22 +1,28 @@
 from flask import Flask
 from flask_restful import Api
-from flask_sqlalchemy import SQLAlchemy
 from resources.challenge import Challenge
 from resources.challenges import Challenges
 from resources.days import Days
+from model.user_model import db
+from model.user_model import User
+
 
 # Flask app
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////projects/challenger.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chalenger.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
+db.init_app(app)
 #from model.user_model import User
 
 # Api app
 api = Api(app)
-
+with app.app_context():
+    db.create_all()
+    user = User(username="Arthur",email="artae@aekhaze.com")
+    session = db.session
+    session.add(user)
+    print User.query.all()
 # add route
 api.add_resource(Challenges, '/challenges/')
 api.add_resource(Challenge, '/challenges/<int:challenge_id>/')
